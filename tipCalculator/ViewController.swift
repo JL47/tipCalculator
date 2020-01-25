89d019a4e11852b6faf0f40c65c9b7819c8104a1
JorgeL47 <jorlopez@csumb.edu>
2020-01-25 13:42:04 -0800

    Initial Commit

---
+++ tipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipCalculator
+//
+//  Created by Jorge Gaytan on 1/25/20.
+//  Copyright © 2020 Jorge Gaytan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
